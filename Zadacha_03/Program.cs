a3cfab9eec834af45e9e710a145021cae673e813
Kirill <Ki_220191@mail.ru>
2023-02-02 00:00:41 +0300

    домашка 4

---
+++ Zadacha_03/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+Console.WriteLine("введите первое число");
+string numberStr1 = Console.ReadLine();
+int a = Convert.ToInt32(numberStr1);
+
+Console.WriteLine("введите второе число");
+string numberStr2 = Console.ReadLine();
+int b = Convert.ToInt32(numberStr2);
+
+Console.WriteLine($"{a} в степени {b} = " + Math.Pow(a, b));
---
+++ Zadacha__01/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] arr = new int[8];
+Random rand = new Random();
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = rand.Next(30); // макс значение элемента в массиве
+        Console.Write($" {array[i]}, ");
+
+    }
+    Console.Write("]");
+
+}
+PrintArray(arr);
